dbb6ec250b4db9b6a1c388280841c3f6161be3ad
Leandro Almeida <leandrotrovilho@gmail.com>
2021-01-19 15:53:43 -0300

    Add método de autenticação

--- backend/src/main/java/com/devjapa/minhasfinancas/resources/UsuarioResource.java
+++ backend/src/main/java/com/devjapa/minhasfinancas/resources/UsuarioResource.java
@@ -16,29 +16,36 @@ import com.devjapa.minhasfinancas.services.UsuarioService;
 @RestController
 @RequestMapping("/api/usuarios")
 public class UsuarioResource {
-	
+
 	private UsuarioService service;
-	
+
 	public UsuarioResource(UsuarioService service) {
 		this.service = service;
 	}
-	
+
+	@PostMapping("/autenticar")
+	public ResponseEntity autenticar(@RequestBody UsuarioDTO dto) {
+		try {
+			Usuario usuarioAutenticado = service.autenticar(dto.getEmail(), dto.getSenha());
+			return ResponseEntity.ok(usuarioAutenticado);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+
 	@PostMapping
 	public ResponseEntity salvar(@RequestBody UsuarioDTO dto) {
-		
-		Usuario usuario = Usuario.builder()
-				.nome(dto.getNome())
-				.email(dto.getEmail())
-				.senha(dto.getSenha()).build();
-		
+
+		Usuario usuario = Usuario.builder().nome(dto.getNome()).email(dto.getEmail()).senha(dto.getSenha()).build();
+
 		try {
 			Usuario salvarUsuario = service.salvarUsuario(usuario);
 			return new ResponseEntity(salvarUsuario, HttpStatus.CREATED);
-			
-		}catch (RegraNegocioException e) {
+
+		} catch (RegraNegocioException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
-		
+
 	}
-	
+
 }
